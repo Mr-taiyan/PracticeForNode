29c410c37bcf2161fcfb785957c2a52e37259a8c
chen <cty9388@hotmail.com>
2018-12-26 15:48:36 +0800

    update test_currency

---
+++ my_first_module/test_currency.js
@@ -0,0 +1,7 @@
+const currency = require('./currency');
+
+console.log('convert 50 Canadian dolloars to the amount of US dollars:');
+console.log(currency.canadianToUS(50));
+
+console.log('30 US dollars equals this amount of Canadian dollars:');
+console.log(currency.USToCanadian(30));
