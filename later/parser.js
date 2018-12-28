7f5f9cd1fba502f7dec266eacf9996255a8c0373
chen <cty9388@hotmail.com>
2018-12-28 10:13:43 +0800

    installed body-parser and made post function

---
+++ later/parser.js
@@ -0,0 +1,29 @@
+const express = require('express');
+app = express();
+const bodyParser = require('body-parser');
+const articles = [{
+    title: 'Example'
+}];
+
+app.set('port', process.env.PORT || 8080);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/articles', (req, res) => {
+    const article = {
+        title: req.body.title
+    };
+    articles.push(article);
+    res.send(articles);
+});
+
+app.get('/articles', (req, res) => {
+    res.send(articles);
+});
+
+app.listen(app.get('port'), () => {
+    console.log('listen at localhost: ', app.get('port'))
+});
