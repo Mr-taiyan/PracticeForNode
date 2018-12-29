69b880a7f94e077632cd2b550390e770f66f3134
chen <cty9388@hotmail.com>
2018-12-29 20:04:44 +0800

    add a excellent event_eimitter example

---
+++ emitter/event_emitter.js
@@ -0,0 +1,27 @@
+"use strict";
+const net = require('net');
+const events = require('events');
+const channel = new events.EventEmitter();
+
+channel.clients = {};
+channel.subscriptions = {};
+channel.on('join', function (id, client) {
+    this.clients[id] = client;
+    this.subscriptions[id] = (senderId, message) => {
+        if (id !== senderId) {
+            this.clients[id].write(message);
+        }
+    };
+    this.on('broadcast', this.subscriptions[id]);
+});
+
+const server = net.createServer(client => {
+    const id = `${client.remoteAddress}:${client.remotePort}`;
+    channel.emit('join', id, client);
+    client.on('data', data => {
+       data = data.toString();
+       channel.emit('broadcast', id, data);
+    });
+});
+
+server.listen(8080);
