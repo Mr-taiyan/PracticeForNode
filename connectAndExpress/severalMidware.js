79dd9f7a5e06c7008cf062594d4bd4e65c76d376
chen <cty9388@hotmail.com>
2019-02-16 10:57:01 +0800

    add error handler file

--- connectAndExpress/severalMidware.js
+++ connectAndExpress/severalMidware.js
@@ -1,15 +1,20 @@
 const connect = require('connect');
+const errorHandler = require('./errHandler/errorHandler.js');
+
 function logger(req, res, next) {
     console.log('%s %s', req.method, req.url);
     next();
 }
 
-function hello(req, res) {
+function hello(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('hello world');
+    ceshi();
+    next();
 }
 
 connect()
     .use(logger)
     .use(hello)
+    .use(errorHandler)
     .listen(3000);
